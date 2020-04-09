a16cda7bc97a0d8c9e1213fd5889f2fe6e46f32d
Ivo Evans <ievans.storrie@gmail.com>
2020-04-09 21:39:46 +0100

    Creat simple, working version

---
+++ compile.js
@@ -0,0 +1,24 @@
+const submitButton = document.querySelector('#submit')
+const inputArea = document.querySelector('#code-area')
+const outputArea = document.querySelector('#output-field')
+
+console.log = (...arguments) => {
+  arguments.forEach(argument => {
+    const newLine = document.createElement('PRE')
+    const codeElement = document.createElement('CODE')
+    const textNode = document.createTextNode(argument)
+    codeElement.appendChild(textNode);
+    newLine.appendChild(codeElement);
+    outputArea.appendChild(newLine);
+    // somewhat unnecessarily elaborate, since console log always logs on a sinle line, no?
+  })
+}
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  try {
+    eval(inputArea.value)
+  } catch {
+    console.log('Error')
+  }
+})
